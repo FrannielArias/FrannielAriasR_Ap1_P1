ff25d4456eedb9e4102da4541891d02baf3ba835
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-09-23 19:03:38 -0400

    Creando el Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using FrannielAriasR_Ap1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrannielAriasR_Ap1_P1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto (DbContextOptions<Contexto> options) 
+        : base(options){ }
+
+    public DbSet<Registro> Registros { get; set; }
+}
