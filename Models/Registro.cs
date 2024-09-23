dc9967cbf24c2fa8824415c283b4a631e0f5e84c
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-09-23 19:03:51 -0400

    Creando el modelo

---
+++ Models/Registro.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrannielAriasR_Ap1_P1.Models;
+
+public class Registro
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+}
