ed0bee0d27aa5f00dfe80a19f636e7f65fe68f90
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-10-12 12:29:38 -0400

    Agregue el conxteto de cobros

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,13 +1,29 @@
 ﻿using FrannielAriasR_Ap1_P1.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace FrannielAriasR_Ap1_P1.DAL;
-
-public class Contexto : DbContext
+namespace FrannielAriasR_Ap1_P1.DAL
 {
-    public Contexto (DbContextOptions<Contexto> options) 
-        : base(options){ }
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) 
+            : base(options) { }
 
-    public DbSet<Prestamos> Prestamos { get; set; }
+        public DbSet<Cobros> Cobro { get; set; }
+        public DbSet<CobrosDetalle> CobroDetalle { get; set; }
+        public DbSet<Prestamos> Prestamos { get; set; }
+        public DbSet<Deudores> Deudores { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Deudores>().HasData(new List<Deudores>()
+            {
+                new Deudores() { DeudorId=1, Nombres="Lia"},
+                new Deudores() { DeudorId=2, Nombres="DjMarte"},
+                new Deudores() { DeudorId=3, Nombres="Franniel"},
+                new Deudores() {DeudorId=4, Nombres="Ronel" }
+
+            });
+        }
+    }
 }
