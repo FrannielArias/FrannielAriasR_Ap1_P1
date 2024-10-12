5a46d6b3b8eeab8e3936e63c0de478047af51135
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-10-12 12:31:31 -0400

    Cree el modelo de prestamos

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FrannielAriasR_Ap1_P1.Models;
 
@@ -7,15 +8,24 @@ public class Prestamos
     [Key]
     public int PrestamoId { get; set; }
 
-    [Required(ErrorMessage = "Este campo es obligatorio.")]
-    [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Solo se permiten letras en este campo.")]
-    public string? Deudor { get; set; }
-
-    [Required(ErrorMessage = "Este campo es obligatorio")]
-    [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Solo se permiten letras en este campo.")]
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
     public string? Concepto { get; set; }
 
-    [Required(ErrorMessage = "Este campo es obligatorio.")]
-    [Range(minimum: 0.1, maximum: 9999999999999, ErrorMessage = "No se permiten valores menores a 0")]
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [Range(minimum: 0.1, maximum: 9999999999, ErrorMessage = "Ingrese un numero valido")]
     public decimal Monto { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [Range(minimum: 0.1, maximum: 9999999999, ErrorMessage = "Ingrese un número válido. ")]
+    public decimal Balance { get; set; }
+
+    public int CobroId { get; set; }
+    [ForeignKey("CobroId")]
+    public Cobros? Cobros { get; set; }
+
+    public int DeudorId { get; set; }
+    [ForeignKey("DeudorId")]
+    public Deudores? Deudores { get; set; }
+
 }
