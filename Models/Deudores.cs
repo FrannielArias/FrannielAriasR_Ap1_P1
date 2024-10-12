85db004b5890dc233740725d05a3ab3d98c76e36
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-10-12 12:31:15 -0400

    Cree el modelo de deudores

---
+++ Models/Deudores.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrannielAriasR_Ap1_P1.Models
+{
+    public class Deudores
+    {
+        [Key]
+        public int DeudorId { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+        public string? Nombres { get; set; }
+    }
+}
