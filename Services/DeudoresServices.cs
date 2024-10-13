9dac88ba7bbd6844293bfd7c1bc9365be43e8d44
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-10-13 01:29:26 -0400

    Creando servicios de deudores y su metodo listar

---
+++ Services/DeudoresServices.cs
@@ -0,0 +1,19 @@
+﻿using FrannielAriasR_Ap1_P1.DAL;
+using FrannielAriasR_Ap1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace FrannielAriasR_Ap1_P1.Services
+{
+    public class DeudoresServices(Contexto _contexto)
+    {
+        public async Task<List<Deudores>> Listar(Expression<Func<Deudores, bool>> criterio)
+        {
+            return await _contexto.Deudores
+                .AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
+
+    }
+}
